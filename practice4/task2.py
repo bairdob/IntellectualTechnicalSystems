25fefaf7595eade51646c0e0c8570bc5da13a472
bairdob <krumane@mail.ru>
2022-12-17 16:44:46 +0300

    refactor moved some logic into functions

--- practice4/task2.py
+++ practice4/task2.py
@@ -7,40 +7,38 @@ def read_file(path: str) -> list:
     return data
 
 
+def cloning_repo(url: str, folder: str):
+    """
+    клонирует репозиторий по ссылке в папку
+    """
+    try:
+        print('cloning {}... into {}'.format(url, folder))
+        Repo.clone_from(url, folder)
+    except Exception as e:
+        pass
+
+def logging(url: str):
+    """
+    логирует результаты клонирования 
+    """
+    if Repo(git_url):
+        with open(log_file, 'a') as f:
+            f.write(git_url + ' OK\n')
+    # TODO git.exc.NoSuchPathError
+    else:
+        with open(log_file, 'a') as f:
+            f.write(git_url + ' FAIL\n')
+
+
 if __name__ == '__main__':
     log_file = 'git.log'
     gits = read_file('git.txt')
 
     for git in gits:
         # получаем ссылку репозитория и имя папки (куда клонировать)
-        git = git.rstrip()
-        name = git.rstrip().split('/')[-1] + '-ssh'
+        git_url = git.rstrip()
+        folder = git.rstrip().split('/')[-1] + '-ssh'
 
-        try:
-            # clone via ssh (will use default keys)
-            print('cloning {}... into {}'.format(git,name))
-            result = Repo.clone_from(git, name) 
-            print(result)
-        except Exception as e:
-            # pass
-            print(e)
-            
-        # логируем результаты клонирования
-        if Repo(name):
-            with open(log_file, 'a') as f:
-                f.write(name + ' OK\n')
-        # TODO git.exc.NoSuchPathError 
-        else:
-            with open(log_file, 'a') as f:
-                f.write(name + ' FAIL\n')
-
-        # try:
-        #     Repo(name)
-        #     with open(log_file, 'a') as f:
-        #         f.write(name + ' OK\n')
-        # # TODO git.exc.NoSuchPathError 
-        # except NoSuchPathError:
-        #     with open(log_file, 'a') as f:
-        #         f.write(name + ' FAIL\n')
-        
+        cloning_repo(git_url, folder)
+        logging(git_url)
 
